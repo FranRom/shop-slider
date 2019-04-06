9ca507b5e7b21422902b6df7b670540133b4fe2f
Fran Rom <franciscoromero@spotahome.com>
2019-04-06 21:24:47 +0200

    Add styles to Slide Component

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Slide.scss';
+
 const Slide = ({ productData }) => {
   const {
     title,
@@ -14,16 +16,39 @@ const Slide = ({ productData }) => {
     image
   } = productData;
   return (
-    <div>
-      <p>{title}</p>
-      <p>{subtitle}</p>
-      <p>{description}</p>
-      <p>{aroma}</p>
-      <p>{taste}</p>
-      <p>{weight}</p>
-      <p>{mouthfeel}</p>
-      <p>{price}</p>
-      <img src={image} alt={title} />
+    <div className="wrapper">
+      <div className="half-column orange">
+        <div>
+          <img src={image} alt={title} />
+        </div>
+      </div>
+      <div className="half-column pink">
+        <span className="block subtitle">{subtitle}</span>
+        <h1 className="block title">{title}</h1>
+        <div className="details">
+          <div className="block description">{description}</div>
+          <div className="block info">
+            <h4>Taste</h4>
+            {taste}
+          </div>
+          <div className="block info">
+            <h4>Aroma</h4>
+            {aroma}
+          </div>
+          <div className="block info">
+            <h4>Mouthfeel</h4>
+            {mouthfeel}
+          </div>
+        </div>
+        <span className="block weight">{weight}</span>
+        <footer className="footer">
+          <div className="price">{price}</div>
+          <div>
+            <input type="number" defaultValue="1" />
+            <button className="btn">ADD TO CART</button>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
--- src/endpoint.js
+++ src/endpoint.js
@@ -4,14 +4,13 @@ export const ENDPOINT_RESPONSE = [
     title: 'Wuyi Water Golden Turtle',
     subtitle: 'Oolong Tea',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
     taste: 'Butterscotch Peanut Butter Longan Fruit',
     aroma: 'Fudge Brownie Cedar Roasted Peanuts',
     mouthfeel: 'Full but short with a mineral finish',
     weight: '80G',
     price: '$39.00',
-    image:
-      'https://www.dawesdesign.com.au/wp-content/themes/Corsa/img/placeholder/600x900.gif'
+    image: 'http://placehold.jp/450x650.png'
   }
 ];
 
